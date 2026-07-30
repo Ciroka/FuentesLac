4187d6a81934bad2b435a84a3e05eb972227c0f5
ciro zavattaro <cirozavattarocampos@gmail.com>
2026-07-30 02:42:40 -0300

    feat: profile + refactor estetica y bugs

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -6,14 +6,14 @@ import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
   imports: [
     FormsModule, RouterLink, MatCard, MatCardContent, MatFormField, MatLabel, MatPrefix, MatSuffix,
-    MatInput, MatIcon, MatButton, MatProgressSpinner
+    MatInput, MatIcon, MatButton, MatProgressSpinner, MatButtonModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
